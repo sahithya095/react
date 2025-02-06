1f25fe68137851453d64593f13d39d3ebca81f84
sahithya095 <2211CS010148@mallareddyuniversity.ac.in>
2025-02-06 10:09:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+const Welcome=()=><h1 className='message'>Hello Sai</h1>
+function App(){
+  return (
+    <div className='App'>
+      <Welcome/>
+    </div>
+  )
+}
+export default App;
