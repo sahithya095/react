9eac2d0cf56ba6b2d032d88ba540a6411be57c58
sahithya095 <2211CS010148@mallareddyuniversity.ac.in>
2025-02-13 10:31:19 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,17 +1,37 @@
-import React from "react";
-const FruitList=()=>{
-  const fruits=['apple','cherry','banana'];
-  return(
-    <div>
-      <h1>fruit list</h1>
-      <ul>
-        {fruits.map((fruit,index)=>(
-          <li key={index}>{fruit}</li>
-        ))
-        }
-      </ul>
-    </div>
-  )
+import React from 'react';
+
+function App() {
+  const user = { name: "D. Sahithya", email: "sahithya@gmail.com" };
+  return (
+    <User1 user={user} />
+  );
 }
 
-export default FruitList;
+function User1({ user }) {
+  return (
+    <User2 user={user} />
+  );
+}
+
+function User2({ user }) {
+  return (
+    <User3 user={user} />
+  );
+}
+
+function User3({ user }) {
+  return (
+    <Profile user={user} />
+  );
+}
+
+function Profile({ user }) {
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>{user.email}</p>
+    </div>
+  );
+}
+
+export default App;
